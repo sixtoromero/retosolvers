7996409d379d538c96b613c7b0f159432cc49500
Sixto José Romero Martínez <sixto.jose@gmail.com>
2019-09-24 23:18:05 -0500

    Se crea el primer Proyecto a la solución Common

---
+++ TechAndSolve.LazyLoad/TechAndSolve.LazyLoad.Common/Clases/Helpers/FileHelper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace TechAndSolve.LazyLoad.Common.Clases.Helpers
+{
+    public static class FileHelper
+    {
+        /// <summary>
+        /// Método que permite obtener los registro del archivo
+        /// </summary>
+        /// <param name="path">Es la ruta donde se encuentra el archivo a leer</param>
+        /// <returns>Retorna la lista del archivo leido</returns>
+        public static List<int> getFile(string path)
+        {
+            try
+            {
+                List<int> listFile = new List<int>();
+                using (StreamReader reader = new StreamReader(path, false))
+                {
+                    var line = string.Empty;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        listFile.Add(Convert.ToInt32(line));
+                    }
+                    reader.Close();
+                }
+                return listFile;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static bool setFile(string path, string result)
+        {
+            try
+            {
+                using (StreamWriter write = new StreamWriter(path))
+                {
+                    write.WriteLine(result);
+                }
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+    }
+}
---
+++ TechAndSolve.LazyLoad/TechAndSolve.LazyLoad.Common/Clases/Helpers/ValidateHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TechAndSolve.LazyLoad.Common.Clases.Helpers
+{
+    public static class ValidateHelper
+    {
+        /// <summary>
+        /// Validación de los pesos
+        /// </summary>
+        /// <param name="valid">T, N, Wi</param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool Validate(string valid, int value)
+        {
+            switch (valid)
+            {
+                case "T":
+                    return value >= 1 && value <= int.Parse(RanksResources.T);
+                case "N":
+                    return value >= 1 && value <= int.Parse(RanksResources.N);
+                case "Wi":
+                    return value >= 1 && value <= int.Parse(RanksResources.Wi);
+                default:
+                    return false;
+            }
+        }
+
+        public static bool IsPair(int Number)
+        {
+            return Convert.ToBoolean((Number % 2 == 0 ? true : false));
+        }
+
+    }
+}
