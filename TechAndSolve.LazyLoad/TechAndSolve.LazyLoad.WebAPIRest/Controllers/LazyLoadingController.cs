c84dc3dc8fd65fa0a6e91a87b2fb2a60b960ce44
Sixto José Romero Martínez <sixto.jose@gmail.com>
2019-09-24 23:27:39 -0500

    Se crea el tercer proyecto que es la API

---
+++ TechAndSolve.LazyLoad/TechAndSolve.LazyLoad.WebAPIRest/Controllers/LazyLoadingController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TechAndSolve.LazyLoad.WebAPIRest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LazyLoadingController : ControllerBase
+    {
+        [HttpGet("{path}/{document}")]
+        public ActionResult<IEnumerable<string>> Get(string path, string document)
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
